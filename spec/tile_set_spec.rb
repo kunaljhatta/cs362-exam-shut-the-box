b57e422d0dea1e34943cd7e7d71864435c72ad06
KunalJhatta <Kunaljhatta22@gmail.com>
2022-03-17 20:00:11 -0700

    passed tilest specified length

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -10,7 +10,8 @@ describe 'A tile set' do
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      length = 7
+      expect(TileSet.generate(length).length).to eq(7)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
